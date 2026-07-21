3133b77b60e9598ec22ad82ab1f9f8f75b49faf0
Naren <karthikeyannaren751@gmail.com>
2026-07-21 22:31:48 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import predict_crop,ask_question
-
-urlpatterns = [
-    path('predict/', predict_crop, name='predict'),
-    path("ask/", ask_question, name='ask'),
-]
